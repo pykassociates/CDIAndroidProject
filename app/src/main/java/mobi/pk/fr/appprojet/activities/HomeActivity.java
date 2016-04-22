9b6da89ef496354d593d325b67f9e07c380373d8
pykassociates <pyk.associates@gmail.com>
2016-04-22 10:56:50 +0200

    Add Schedule Activity and button in home to navigate to proximity

--- app/src/main/java/mobi/pk/fr/appprojet/activities/HomeActivity.java
+++ app/src/main/java/mobi/pk/fr/appprojet/activities/HomeActivity.java
@@ -1,7 +1,10 @@
 package mobi.pk.fr.appprojet.activities;
 
-import android.os.Bundle;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import mobi.pk.fr.activities.R;
 
@@ -11,5 +14,15 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // buttonProximity
+        Button buttonProximity = (Button) this.findViewById(R.id.buttonProximity);
+        buttonProximity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent proximity = new Intent(v.getContext(), ProximityActivity.class);
+                startActivity(proximity);
+            }
+        });
     }
 }
---
+++ app/src/main/java/mobi/pk/fr/appprojet/activities/ScheduleActivity.java
@@ -0,0 +1,15 @@
+package mobi.pk.fr.appprojet.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import mobi.pk.fr.activities.R;
+
+public class ScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
