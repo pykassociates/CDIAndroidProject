202e9bf543db6f43512c69178a96f6e50f38ddd2
pykassociates <pyk.associates@gmail.com>
2016-05-10 13:26:10 +0200

    ProximityActivity

--- app/src/main/java/mobi/pk/fr/appprojet/activities/ProximityActivity.java
+++ app/src/main/java/mobi/pk/fr/appprojet/activities/ProximityActivity.java
@@ -1,5 +1,12 @@
 package mobi.pk.fr.appprojet.activities;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -20,7 +27,8 @@ public class ProximityActivity extends FragmentActivity implements OnMapReadyCal
 
     private GoogleMap mMap;
     private LatLng position;
-    private List<Station> stations = new ArrayList<Station>();;
+    private List<Station> stations = new ArrayList<Station>();
+    private LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,13 +58,58 @@ public class ProximityActivity extends FragmentActivity implements OnMapReadyCal
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        position = new LatLng(45.191513, 5.714254);
-        //for (Station station: stations) {
-        //    LatLng position = new LatLng(station.getLocation().getLatitude(), station.getLocation().getLongitude());
-        //    mMap.addMarker(new MarkerOptions().position(position).title(station.getName()));
-        //}
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 5));
+        // Move the camera to Grenoble
+        // position = new LatLng(45.191513, 5.714254);
+        // Move the camera to last known location
+        GetGPSPosition();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 12));
+        mMap.addMarker(new MarkerOptions().position(position));
 
+        //Add markers to stations
+        for (Station station : stations) {
+            LatLng position = new LatLng(station.getLocation().getLatitude(), station.getLocation().getLongitude());
+            mMap.addMarker(new MarkerOptions().position(position).title(station.getName()));
+        }
+    }
+
+    public void GetGPSPosition() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            position = new LatLng(45.191513, 5.714254);
+            return;
+        }
+        Location lastKnownLocation = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+        position = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+
+        /*locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 1000, 1, new LocationListener() {
+            @Override
+              public void onLocationChanged(Location location) {
+                if (location.getAccuracy() < 10){
+
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        });*/
     }
 }
