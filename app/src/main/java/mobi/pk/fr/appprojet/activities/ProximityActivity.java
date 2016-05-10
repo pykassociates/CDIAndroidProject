0e2d8f6bc0685bc42fa3d3dd887c44f60a72b314
pykassociates <pyk.associates@gmail.com>
2016-05-10 14:03:33 +0200

    ProximityActivity

--- app/src/main/java/mobi/pk/fr/appprojet/activities/ProximityActivity.java
+++ app/src/main/java/mobi/pk/fr/appprojet/activities/ProximityActivity.java
@@ -74,7 +74,7 @@ public class ProximityActivity extends FragmentActivity implements OnMapReadyCal
 
     public void GetGPSPosition() {
         LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
