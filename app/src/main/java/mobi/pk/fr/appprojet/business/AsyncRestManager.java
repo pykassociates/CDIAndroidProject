0912a74c64d7678aa2eeaf4702d8c066e249e9fa
pykassociates <pyk.associates@gmail.com>
2016-05-10 16:28:00 +0200

    Async calls implementation for stations retrieving

--- app/src/main/java/mobi/pk/fr/appprojet/business/AsyncRestManager.java
+++ app/src/main/java/mobi/pk/fr/appprojet/business/AsyncRestManager.java
@@ -1,29 +1,39 @@
 package mobi.pk.fr.appprojet.business;
 
 import android.os.AsyncTask;
+import android.support.annotation.NonNull;
 
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import mobi.pk.fr.appprojet.entity.Station;
 
 /**
  * Created by kpareau on 10/05/2016.
  */
-public class AsyncRestManager extends AsyncTask<String, Integer, List<Object>> {
+public class AsyncRestManager extends AsyncTask<String, Integer, Map<String, String>> {
 
-    private List<Station> stations;
     private RestClient restClient;
 
-    public AsyncRestManager(List<Station> stations) {
-        this.stations = stations;
+    public AsyncRestManager() {
         restClient = new RestClient();
     }
 
     @Override
-    protected List<Object> doInBackground(String... urls) {
-        String url = urls[0];
+    protected Map<String, String> doInBackground(String... params) {
+        String url = params[0];
         String stationsAsJson = restClient.sendGetRequest(url);
-        return null;
+        Map<String, String> mapToReturn = new HashMap<String, String>();
+        mapToReturn.put(params[1], stationsAsJson);
+        return mapToReturn;
+    }
+
+    // Called when doInBackground terminate
+    protected void onPostExecute(Map<String, String> map) {
+        //TODO : notify Metro
     }
 
 }
