39e94988fedb32328bea4a227636f531e43eabca
pykassociates <pyk.associates@gmail.com>
2016-05-31 15:34:56 +0200

    On a un code qui marche (ou pas)

--- app/src/main/java/mobi/pk/fr/appprojet/activities/ProximityActivity.java
+++ app/src/main/java/mobi/pk/fr/appprojet/activities/ProximityActivity.java
@@ -41,28 +41,11 @@ public class ProximityActivity extends FragmentActivity implements OnMapReadyCal
 
     private MetroMobiliteAPIAdapter metroMobiliteAPIAdapter;
 
-    private BroadcastReceiver receiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            Log.i(getClass().getName(), "Receiving Broadcast");
-            String result = intent.getStringExtra(Consts.RESULT_AS_JSON);
-            Log.i(getClass().getName(), "Result value : " + result);
-            Toast.makeText(context, "OK, ça marche : ", Toast.LENGTH_LONG).show();
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Get the JSON from intent
-        //Intent proximity = this.getIntent();
-        //proximity.getStringExtra(Consts.RESULT_AS_JSON);
-        //Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
-
-
         position = new LatLng(45.191513, 5.714254);
-        registerReceiver(receiver, new IntentFilter(Consts.STRING_FOR_INTENT_CALLBACK));
 
         setContentView(R.layout.activity_proximity);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -71,87 +54,6 @@ public class ProximityActivity extends FragmentActivity implements OnMapReadyCal
         mapFragment.getMapAsync(this);
 
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-
-        GetGPSPosition();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
-            return;
-        }
-        locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 10000, 10, locationListener);
-
-        //TODO : chercher stations
-        //TODO : remplir stations
-    }
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-
-        // Move the camera to Grenoble
-        // position = new LatLng(45.191513, 5.714254);
-        // Move the camera to last known location
-        //GetGPSPosition();
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 16));
-        mMap.addMarker(new MarkerOptions().position(position));
-
-        metroMobiliteAPIAdapter = new MetroMobiliteAPIAdapter(mMap, this);
-        metroMobiliteAPIAdapter.findNearestStations(position);
-
-        //Add markers to stations
-        for (Station station : stations) {
-            LatLng position = station.getLocation();
-            //LatLng position = new LatLng(station.getLocation().latitude(), station.getLocation().longitude());
-            mMap.addMarker(new MarkerOptions().position(position).title(station.getName()));
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        registerReceiver(receiver, new IntentFilter(Consts.STRING_FOR_INTENT_CALLBACK));
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        unregisterReceiver(receiver);
-    }
-
-    public void GetGPSPosition() {
-        position = new LatLng(45.191513, 5.714254);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
-            return;
-        }
-        Location lastKnownLocation = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
-        if (lastKnownLocation != null){
-            position = new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
-        }
 
         locationListener = new LocationListener(){
             @Override
@@ -191,5 +93,72 @@ public class ProximityActivity extends FragmentActivity implements OnMapReadyCal
                         Toast.LENGTH_SHORT ).show();
             }
         };
+
+        LatLng currentPosition = getGPSPosition();
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+            return;
+        }
+
+        locationManager.requestLocationUpdates(locationManager.GPS_PROVIDER, 10000, 10, locationListener);
+
+        //TODO : chercher stations
+        //TODO : remplir stations
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 16));
+        mMap.addMarker(new MarkerOptions().position(position));
+
+        metroMobiliteAPIAdapter = new MetroMobiliteAPIAdapter(mMap, this);
+        metroMobiliteAPIAdapter.findNearestStations(getGPSPosition());
+
+        //Add markers to stations
+        for (Station station : stations) {
+            LatLng position = station.getLocation();
+            //LatLng position = new LatLng(station.getLocation().latitude(), station.getLocation().longitude());
+            mMap.addMarker(new MarkerOptions().position(position).title(station.getName()));
+        }
+    }
+
+    public LatLng getGPSPosition() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 200);
+            return null;
+        }
+        Location lastKnownLocation = locationManager.getLastKnownLocation(locationManager.GPS_PROVIDER);
+        if (lastKnownLocation != null){
+            return new LatLng(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+        }
+        return new LatLng(45.191513, 5.714254);
     }
 }
